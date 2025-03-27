b79922b06dfe772aca4af94fa3803623280dc349
Nikhil <nikhilgupta932003@gmail.com>
2025-03-28 04:12:54 +0530

    added type prblem and basic design

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
-// hi this is nikhil
+package main
+
+import("")
+
+
+func problemPuller(fileName string)([]problem,error){
+
+}
+
+func main(){
+
+
+}
+
+type problem struct{
+	q string
+	a string
+}
